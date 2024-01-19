bf442f4b642301420e89bef2ae4d4d0d25c57e56
Ricardo Rocha <ricardo.fabio.rocha@gmail.com>
2024-01-19 15:34:29 -0300

    Added delete function and altered other functions to work with an ID that is exclusive to each entry

--- cashCrash.py
+++ cashCrash.py
@@ -1,5 +1,5 @@
 import json
-
+ID = 0
 def ler_dados():
     try:
         with open('registros.json', 'r') as arquivo:
@@ -13,21 +13,33 @@ def salvar_dados(dados):
     with open('registros.json', 'w') as arquivo:
         json.dump(dados, arquivo, indent=2)
 
-def criar_registro(nome, idade):
-    return {'nome': nome, 'idade': idade}
+def criar_registro(ID, nome, idade):
+    return {'ID':ID,'nome': nome, 'idade': idade}
 
 def listar_registros():
     dados = ler_dados()
     for i, registro in enumerate(dados, 1):
-        print(f"{i}. Nome: {registro['nome']}, Idade: {registro['idade']}")
+        print(f"{i}. ID:{registro['ID']} Nome: {registro['nome']}, Idade: {registro['idade']}")
 
-def adicionar_registro(nome, idade):
+def adicionar_registro(ID, nome, idade):
     dados = ler_dados()
-    novo_registro = criar_registro(nome, idade)
+    novo_registro = criar_registro(ID, nome, idade)
     dados.append(novo_registro)
     salvar_dados(dados)
     print("Registro adicionado com sucesso!")
 
+def deletar_registro(dados, index_delete):
+        index_to_delete = None
+        for i, item in enumerate(dados):
+            if item.get('ID') == index_delete:
+                index_to_delete = i
+                break
+        # If the element is found, delete it
+        if index_to_delete is not None:
+            del dados[index_to_delete]
+        else:
+            print(f"Element with ID {index_delete} not found.")
+        salvar_dados(dados)
 # Exemplo de utilização
 while True:
     print("\n1. Listar Registros")
@@ -41,7 +53,17 @@ while True:
     elif escolha == '2':
         nome = input("Digite o nome: ")
         idade = input("Digite a idade: ")
-        adicionar_registro(nome, idade)
+        ID = ID + 1
+        adicionar_registro(ID, nome, idade)
+    elif escolha == '3':
+        index_delete = int(input("Insira o ID da entrada que deseja remover"))
+        ler_dados()
+        try:
+            with open('registros.json', 'r') as arquivo:
+                dados = json.load(arquivo)
+        except FileNotFoundError:
+            dados = []        
+        deletar_registro(dados, index_delete)
     elif escolha == '5':
         break
     else:
