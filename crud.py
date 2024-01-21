5e1f0d92cf93372c254a10734bc9abea9e06af7a
Ricardo Rocha <ricardo.fabio.rocha@gmail.com>
2024-01-21 00:59:42 -0300

    Edição e deleção completamente prontos, o json e o populate precisam ser modificados para aceitar o indexador quando investimentos, porém em relação ao update, é possivel de atualizar isto posteriormente em 5 minutos

--- crud.py
+++ crud.py
@@ -1,7 +1,9 @@
 import json
 import sys
+from datetime import datetime
 sys.path.append('services\editarOuCancelar\cancelamento')
 import cancelarOperacao
+tipo_edit = {'D':'Despesa', 'R': 'Receita', 'I': 'Investimento'}
 def ler_dados():
     try:
         with open('database/registros.json', 'r') as arquivo:
@@ -12,7 +14,7 @@ def ler_dados():
     return dados
 
 def salvar_dados(dados):
-    with open('registros.json', 'w') as arquivo:
+    with open('database/registros.json', 'w') as arquivo:
         json.dump(dados, arquivo, indent=2)
 
 def criar_registro(nome, idade):
@@ -30,16 +32,49 @@ def adicionar_registro(nome, idade):
     salvar_dados(dados)
     print("Registro adicionado com sucesso!")
 
-#WIP: cancelar registro
-def deletar_registro(dados, id_change):
-        index_to_delete = None
+def editar_registro(dados, id_change):
+        index_change = None
         for i, item in enumerate(dados):
             if item.get('id') == id_change:
-                index_to_delete = i
+                index_change = i
+                break
+
+        if index_change is not None:
+            registro_edit = dados[index_change]
+            print (f"\nRegistro a ser editado: \n {registro_edit}\n")
+            registro_edit_tipo = input("Insira [D] para transformar em despesa, [R] para transformar em receita e [I] para transformar em investimento.\nDeixe em branco para não alterar tipo\n")
+            if registro_edit_tipo in tipo_edit:
+                registro_edit['tipo'] = tipo_edit[registro_edit_tipo]
+            if registro_edit['tipo'] == "Investimento":
+                registro_edit_titulo = input("Insira o titulo atualizado:\n[1] - CDB (0,039%/dia)\n[2] - LCI (0,038%/dia)\n[3] - LCA (0,036%/dia)\n")
+            try:
+                registro_edit_valor = int(input("Insira o valor atualizado do registro.\nDeixe em branco para não alterar\n"))
+            except:
+                print("Valor invalido!")
+            if isinstance(registro_edit_valor, int):
+                registro_edit['valor'] = registro_edit_valor
+            new_date = str(datetime.now())
+            registro_edit['data'] = new_date
+            dados[index_change] = registro_edit
+            salvar_dados(dados)
+            print(f"""----------------------------------
+        EDITADO COM SUCESSO!
+----------------------------------
+A operação de id {id} foi editada com sucesso! Resultado após a edição:
+
+Tipo: {tipo_edit[registro_edit_tipo]}
+Valor: {registro_edit_valor}
+Data: {new_date}""")
+
+def deletar_registro(dados, id_change):
+        index_change = None
+        for i, item in enumerate(dados):
+            if item.get('id') == id_change:
+                index_change = i
                 break
         # If the element is found, delete it
-        if index_to_delete is not None:
-            del dados[index_to_delete]
+        if index_change is not None:
+            del dados[index_change]
             print(f'''
 ----------------------------------
         CANCELADO COM SUCESSO!
--- services/editarOuCancelar/opcoesMenu.py
+++ services/editarOuCancelar/opcoesMenu.py
@@ -1,19 +1,23 @@
 import sys
 sys.path.append('.')
 sys.path.append('services')
+#from menuPrincipal import menuPrincipalController
 from despedida import despedidaController
 from time import sleep
 #import crud
 from crud import deletar_registro
 from crud import ler_dados
+from crud import editar_registro
 import json
 import os
+
 textoOpcoes = '''
 ----------------------------------
     EDITAR/CANCELAR OPERAÇÃO
 ----------------------------------
 Deseja editar ou cancelar? [E/C]
 Digite [1] para sair.
+Digite [2] para voltar ao menu principal
 '''
 def menuEditarOuCancelar():
   while True:
@@ -22,6 +26,10 @@ def menuEditarOuCancelar():
     if editarOuCancelar in ['1', 'um', 'Um', 'UM', '[1]']:
       despedidaController()
       break
+    elif editarOuCancelar in ['2', 'dois', 'DOIS','Dois','[2]','DOis']:
+       import menuPrincipal
+       menuPrincipal.menuPrincipalController()
+       break
     elif editarOuCancelar in ['C', 'c']:
         id_change = input("Insira o ID da operação a ser cancelada")
         try:
@@ -35,6 +43,15 @@ def menuEditarOuCancelar():
             json.dump(dados, arquivo, indent=2)
         os.remove('registros.json')
         break
+    elif editarOuCancelar in ['E', 'e']:
+        id_change = input("Insira o ID da operação a ser editada")
+        try:
+            with open('database/registros.json', 'r') as arquivo:
+                dados = json.load(arquivo)
+        except FileNotFoundError:
+            dados = []
+        editar_registro(dados, id_change)
+        
     else:
       print("\nESCOLHA INVÁLIDA.\nDigite [E] para editar uma operação, [C] para cancelar uma operação ou [1] para sair.\n")
       sleep(4)
