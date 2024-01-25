57a4ed523826f1b40911f4bbe1970f22d8515646
DennisRPaiva <dennispaiva1995@gmail.com>
2024-01-25 09:49:22 -0300

    changes to deposito code

--- services/saqueOuDeposito/deposito.py
+++ services/saqueOuDeposito/deposito.py
@@ -1,4 +1,3 @@
-import json
 from crud import adicionar_registro
 from services.saqueOuDeposito.sucessoOperacao import sucessoOperacaoController
 
@@ -12,7 +11,6 @@ Escreva 'voltar' para voltar para o menu principal.
 Insira o valor do depósito:  '''
 
 def iniciarDeposito():
-    from services.menuPrincipal import menuPrincipalController
     entradaUsuario = input(textoMenu)
     while True:
         if entradaUsuario.isnumeric():
@@ -20,7 +18,9 @@ def iniciarDeposito():
             sucessoOperacaoController("DEPOSITO", entradaUsuario)
             break
         elif entradaUsuario == 'voltar':
+            from services.menuPrincipal import menuPrincipalController
             menuPrincipalController()
+            break
         else:
             print('\n\nvalor inválido. Escreva um numero.\n\n')
             textoDeFalha = '''Escreva 'voltar' para voltar para o menu principal.\n\nInsira o valor do depósito:  '''
