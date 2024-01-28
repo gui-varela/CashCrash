8f0720157af74f7514d27873e0c0e8a98eaeba1f
Guilherme Varela <guilhermevareladossantos@gmail.com>
2024-01-27 22:25:34 -0300

    Cria métodos de listagem
    1. listar_registros_por_tipo(tipo)
    
    Lista registros por tipo de operação.
    
    2. listar_registros_por_data_e_valor({parametros})
    
    Filtra os registros por data inicial, data final, valor inicial, valor final, a partir de um conjunto de dados. Os parâmetros são:
    - dados, data_inicial,
    - data_final,
    - valor_inicial,
    - valor_final
    
    #14

--- crud.py
+++ crud.py
@@ -2,7 +2,7 @@ import json
 import sys
 from datetime import datetime
 sys.path.append('services\editarOuCancelar\cancelamento')
-import cancelarOperacao
+import services.editarOuCancelar.cancelamento.cancelarOperacao
 
 
 tipo_edit = {'D':'Despesa', 'R': 'Receita', 'I': 'Investimento'}
@@ -39,6 +39,29 @@ def listar_registros():
     dados = ler_dados()
     for i, registro in enumerate(dados, 1):
         print({'id': {registro['id']}, 'tipo': {registro['tipo']}, 'valor': {registro['valor']}, 'data': {registro['data']}})
+
+def listar_registros_por_tipo(tipo):
+    dados = ler_dados()
+    dados_por_tipo = [ dado for dado in dados if dado['tipo'] == tipo ]
+    return dados_por_tipo
+
+def listar_registros_por_data_e_valor(dados, data_inicial, data_final, valor_inicial, valor_final):
+    dados_filtrados = []
+    
+    for dado in dados:
+        data_formatada = datetime.strptime(dado["data"], '%Y-%m-%d %H:%M:%S')
+
+        data_valida = (not data_inicial or data_formatada >= data_inicial) and \
+                    (not data_final or data_formatada <= data_final)
+        
+        valor_valido = (not valor_inicial or dado["valor"] >= valor_inicial) and \
+                    (not valor_final or dado["valor"] <= valor_final)
+        
+        if data_valida and valor_valido:
+            dados_filtrados.append(dado)
+
+    return dados_filtrados   
+
 
 def adicionar_registro(tipo, valor):
     dados = ler_dados()
