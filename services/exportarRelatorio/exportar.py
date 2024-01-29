487dded0a1eb87a3e2d813dcf439e1bc1189ee2c
macedocoding <macedo@tuta.io>
2024-01-29 12:50:43 -0300

    Pequenos ajustes

--- services/exportarRelatorio/exportar.py
+++ services/exportarRelatorio/exportar.py
@@ -2,8 +2,8 @@ import os
 import shutil
 
 def exportar_relatorio(caminho_database):
-    arquivo_json = "registros.json"
-    caminho_destino = os.path.join(os.path.expanduser("~"), "Downloads", arquivo_json)
+    nome_relatorio = "relatorio.json"
+    caminho_destino = os.path.join(os.path.expanduser("~"), "Downloads", nome_relatorio)
 
     shutil.copy(caminho_database, caminho_destino)
     print(f"Relatório exportado para: {caminho_destino}")
